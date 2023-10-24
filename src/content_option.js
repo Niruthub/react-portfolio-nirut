eb737936b4f94914578862f2bcd62b10bb4c2b78
NirutKunwong <s6303051623055@email.kmutnb.ac.th>
2023-10-24 10:00:13 +0700

    test set data nirut

--- src/content_option.js
+++ src/content_option.js
@@ -1,18 +1,19 @@
-const logotext = "JOHN";
+const logotext = "Nirut Profile";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Nirut Profile",
+    description: "I have dreams of being a full-stack developer.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Nirut Kunwong",
     animated: {
         first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        second: "This website is a template profile.",
+        third: "สวัสดี เราชื่อ นิรุท! 😂",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "เรื่องราวของเด็กหนุ่มที่มีความฝัน อยากเป็นโปรแกรมเมอร์ มีเงินเลี้ยงชีพ ใช้ชีวิตอย่างสงบสุข ",
+    your_img_url: "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/393258177_7040690205987848_4406289063087495909_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE2ixnT2enzBHazA0ozF3SDODByBriOgEE4MHIGuI6AQbSiTOGiA0KCSWsrC0Rk-JoPhH3n3BtXCs7lI4I2Skb8&_nc_ohc=fDy2xKmBBkMAX_Ox0tY&_nc_ht=scontent.fbkk5-4.fna&oh=00_AfCqmYkwbSDUzmxG-pGG9wxqvZ-MShTPt5g23B1Ht751Ow&oe=653A4D84",
+    your_img_url_2: "https://media.discordapp.net/attachments/738013225836347493/1163392030219718757/000051.JPG?ex=6548a2c2&is=65362dc2&hm=15d9a3a9d4cc5b324541b451cd301abe9cdbbba2bd7af12db1b19ab76c49bc97&=&width=1004&height=671",
 };
 
 const dataabout = {
@@ -73,64 +74,64 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.6435-9/101798035_2764667653761405_5461048376343590831_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=dd63ad&_nc_eui2=AeGSO5-sFQ2gP8G0JBwcjFFO6MMcvikc2kzowxy-KRzaTFw1OXhZgEiZJJ22T0hHMrlzV7-cMd_1Wsy5KVCDUYg8&_nc_ohc=Z6qBH1ynkiIAX-bnNDy&_nc_ht=scontent.fbkk5-4.fna&oh=00_AfAyvN4NOlbILJ6S-_3voqAPaTZRDMEsF1xkW-DKovLUAA&oe=655DC5FA",
+        description: "ช่วยงาน รด. ตรวจวินัย",
+        link: "https://www.facebook.com/photo.php?fbid=2764667650428072&set=t.100001408142715&type=3",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://scontent.fbkk5-7.fna.fbcdn.net/v/t1.6435-9/82424047_1211334535734438_8355369995420565504_n.png?_nc_cat=108&ccb=1-7&_nc_sid=7f8c78&_nc_eui2=AeGd5_VcDXQfM2c--MIfJzlw9-9NGBVHJjD3700YFUcmMLVqr1EhJxOLsYW7Nu1TKiOt535AGz_a2Xet4_495KeQ&_nc_ohc=ZjXwEIyRpzQAX8io850&_nc_ht=scontent.fbkk5-7.fna&oh=00_AfDrA-w1ye6bwGeWbeL1iyoT8ld-uuRh_W5WJjearzJhIQ&oe=655DA384",
+        description: "ร่วมแข่งขัน ทักษะการเขียนโปรแกรมควบคุมอุปกรณ์ ของวิทยาลัย",
+        link: "https://www.facebook.com/ct.utcfanpage/photos/t.100001408142715/1211334532401105/?type=3",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.6435-9/86696663_10218418027655211_1023557493378252800_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=0bb214&_nc_eui2=AeEgXTiPK2Az7cMGgOXkb_cCkjLyWV2t9QqSMvJZXa31Cg6aLoY25vosuan3yGD_pPeaOIz2vVHkCC-vuO21_kja&_nc_ohc=5yLlBekcxosAX8DmJQX&_nc_ht=scontent.fbkk5-6.fna&oh=00_AfCvNxW99UqDb4UypQMMklwHrpS3_CirGmYnXucPdi_j0A&oe=655DCB6B",
+        description: "โปรเจคจบ ปวช. เว็บไซต์ ร้านกาแฟ",
+        link: "https://www.facebook.com/photo?fbid=10218418027615210&set=t.100001408142715",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://scontent.fbkk5-1.fna.fbcdn.net/v/t1.6435-9/47358945_2174553179268266_1875051797945516032_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=dd63ad&_nc_eui2=AeFz1ByCEpztByG95MnS_VbmLNVoDYlo92As1WgNiWj3YFZAXBSQ5vRMVT72htNJaEkqVIxW_sr4tHzISvKgWu_N&_nc_ohc=QyWQUIdO_S8AX_qktkh&_nc_ht=scontent.fbkk5-1.fna&oh=00_AfA0fJPWj7quXemr07Bku-FM1ir9bAj93xqv4NuBdtW0pw&oe=655DBC72",
+        description: "เป็นตรวจวินัย จราจร",
+        link: "https://www.facebook.com/photo.php?fbid=2174553175934933&set=pb.100001408142715.-2207520000&type=3",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.18169-9/14900394_1299487856774807_505326348852116195_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeH0-6t2Na-qgN12Z3eby0YdwH1JpLqeSIPAfUmkup5Ig9QiKr6ccsc_CD93spq0nP-YwPBaQaTMHw9uJ8oHbh4y&_nc_ohc=g-X0ImkM_ukAX8-m7HH&_nc_ht=scontent.fbkk5-3.fna&oh=00_AfCLf8JtRjXlTouk4BSX4jly93cW3GTQ3QyXqDwmQsObUw&oe=655DCA6C",
+        description: "เคยเป็น นักดนตรี วงออร์เคสตร้า ของโรงเรียน (ม.ต้น)",
+        link: "https://www.facebook.com/photo.php?fbid=1299487856774807&set=t.100001408142715&type=3",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/340258076_185328467632471_1978862963587104999_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGYur8VDILvn7a5eiwDG0nSBrIdZ2QWO3wGsh1nZBY7fO0YXz_4ayDtG6lWr9ZswZYQA9ZLYcxLkMdXwTLjweE3&_nc_ohc=NiDTFSw3-zoAX9LxZN4&_nc_ht=scontent.fbkk5-4.fna&oh=00_AfA33xpRmf4QEbhODAM1iP7fvOSxYf3k_Gc86A5cNauUxw&oe=653B5608",
+        description: "เรียนวิชา cloud computing",
+        link: "https://www.facebook.com/photo/?fbid=171628355750824&set=pcb.171628442417482",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://scontent.fbkk5-7.fna.fbcdn.net/v/t1.6435-9/57852645_2388787841178131_7043743672495505408_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7f8c78&_nc_eui2=AeERrbBPNmIUhQUfqF5d8yVkescOBN4hzyp6xw4E3iHPKs01h9kg7MKvP-bT8_C5qrtxOVq52-EpuxXgdPx-B6XF&_nc_ohc=ikbW0VWTbnsAX-F3JBJ&_nc_ht=scontent.fbkk5-7.fna&oh=00_AfDLbVBw03K-ZRJNrCz2LWuR7aoCChBu6CIsXchaU8awxg&oe=655DAABF",
+        description: "ช่วงฝึกงาน (ปวช.)",
+        link: "https://www.facebook.com/photo.php?fbid=2388787834511465&set=pb.100001408142715.-2207520000&type=3",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://scontent.fbkk5-8.fna.fbcdn.net/v/t1.6435-9/36086889_1929684970421756_493143141289820160_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=dd63ad&_nc_eui2=AeF0CgnkS-Eq_-PFvW0hx_yBljeFGXaUO92WN4UZdpQ73TPlIp8Hjs-TbdcEN3S-wCoKrrj0PkrIz0gruPGyJgCj&_nc_ohc=1AUCaaifKP0AX-6I-vx&_nc_ht=scontent.fbkk5-8.fna&oh=00_AfCRjkCHzZBmeS0_uersEgu0PulSxgeQTktyD0qWEAO8iw&oe=655DAF26",
+        description: "รด.จิตอาสา",
+        link: "https://www.facebook.com/photo.php?fbid=1929684967088423&set=pb.100001408142715.-2207520000&type=3",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.6435-9/39407859_2019242828132636_4637484675793682432_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=7f8c78&_nc_eui2=AeF2B9SdNXgqzIWRIMemq-Dr3Zn6N4OndQ7dmfo3g6d1DgZP8wXAOCfQP6XFozrmYW7gGhwuB2VJDrF6N5mFEWTa&_nc_ohc=BiC9gwMN3dIAX-RV48F&_nc_ht=scontent.fbkk5-6.fna&oh=00_AfDWzJfSMqRYykgxpbvhBW0i2BMjiHZH2q1wr4RHrUewaw&oe=655DA724",
+        description: "ร่วมอบรม เกษตรอัฉริยะ",
+        link: "https://www.facebook.com/photo?fbid=2019242821465970&set=t.100001408142715",
     },
     {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://react-folio-for-deploy-25r5-mosts-projects-50ed4e6a.vercel.app/project/keptang.png",
+        description: "งาน cloud เว็บบันทึกรายรับ-รายจ่าย",
+        link: "https://github.com/Yunocat02/Keptang",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
+        img: "https://react-folio-for-deploy-25r5-mosts-projects-50ed4e6a.vercel.app/project/LB.png",
+        description: "งานกลุ่ม LITTLE BUDDY Mobile Application",
+        link: "https://github.com/Yunocat02/LittleBuddy",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fbkk5-7.fna.fbcdn.net/v/t1.6435-9/29133370_1810625588994362_2802564077304414208_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=7f8c78&_nc_eui2=AeFXsJewvoNs_kAw8wna3fV-Saxj70Jw-YZJrGPvQnD5hoMxV7QI1MTcw007gh5ywl7bTitl6adjhhWHP8vzrmsy&_nc_ohc=NNQ8wvlvH40AX_pskwv&_nc_ht=scontent.fbkk5-7.fna&oh=00_AfBGtJFWTpvdpqZdtvWYZUeZ_Z8Z0ZMB8dZNk2_AQ4gldA&oe=655DA56A",
+        description: "ทำได้ทุกอย่าง",
         link: "#",
     },
 ];
@@ -147,10 +148,10 @@ const contactConfig = {
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/Niruthub",
+    facebook: "https://www.facebook.com/nirud.kunwong/",
+    linkedin: "https://www.linkedin.com/in/nirutsoftlife-undefined-645839273/",
+    twitter: "https://twitter.com/Nirut_Twiter",
 };
 export {
     meta,
